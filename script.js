8339e0d612f98f12a395c87c3e5d4bea47c93e6d
KDMEHTA704 <131295594+KDMEHTA704@users.noreply.github.com>
2025-02-06 12:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function stringChop(str, size) {
   // your code here
-}
+	let chunks = [];
+    for (let i = 0; i < str.length; i += size) {
+        chunks.push(str.substring(i, i + size)); // Extract chunks of 'size' length
+    }
+    return chunks;
 
-// Do not change the code below
+}
+// // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
